d2a4a1cefe92e3ed1d5f219bbf1130c8b087f99d
Benjamin Danjoux <bdanjoux_github_commit_signing@xsoar.fr>
2022-12-20 16:01:37 +0100

    number widgets - added script1.py

---
+++ CortexXSOAR/number widgets/script1.py
@@ -0,0 +1,78 @@
+FromDate, ToDate = NitroDateFactory.from_regular_xsoar_date_range_args(demisto.args())
+
+is_dashboard = demisto.args().get("widgetType") is not None
+
+from_ = "2022-10-01T00:00:00+00:00"
+to_ = "2022-11-01T00:00:00+00:00"
+
+if is_dashboard:
+    if isinstance(FromDate, NitroRegularDate):
+        from_ = FromDate.to_iso8601()
+    else:
+        from_ = None
+    if isinstance(ToDate, NitroRegularDate):
+        to_ = ToDate.to_iso8601()
+    else:
+        to_ = None
+
+q = "SecurityIncident"
+
+q_ = list()
+
+if from_ is not None:
+    q_.append(f'TimeGenerated >= datetime("{from_}")')
+
+if to_ is not None:
+    q_.append(f'TimeGenerated >= datetime("{to_}")')
+
+if q_:
+    q += "\n| where " + " and ".join(q_)
+
+q += """
+| extend same = 1
+| summarize count() by same"""
+
+
+res = demisto.executeCommand(
+    "azure-sentinel-query",
+    {"query": q}
+)
+
+counts = []
+
+for res_ in res:
+    if not (
+        isinstance(res_, dict)
+        and
+        isinstance(lst := res_.get("Contents"), list)
+    ):
+        continue
+    for lst_ in lst:
+        if (
+            isinstance(lst_, dict)
+            and
+            isinstance(count_ := lst_.get("count_"), int)
+        ):
+            counts.append(count_)
+
+total_count = sum(counts)
+
+data = {
+    "Type": 17,
+    "ContentsFormat": "number",
+    "Contents": {
+        "stats": total_count,
+        "params": {
+            "name": "Incidents Last Month",
+            "colors": {"items": {
+                "green": {"value": 40}
+            }},
+        },
+    },
+}
+
+
+if is_dashboard:
+    demisto.results(total_count)
+else:
+    demisto.results(data)
